1cef0c1edf630e5165ec2fd152d962fb74d64ac2
anishdhakal54 <anish.dhakal54@gmail.com>
2020-02-06 11:09:30 +0545

    Initial commit

---
+++ app/src/main/java/com/anish/app/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.anish.app.Prevalent;
+
+import com.anish.app.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
